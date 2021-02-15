b61347fbbe061cb042719247cd3596e148696009
Kentiy <Kentiy93@gmail.com>
2021-02-15 19:18:17 +0700

    v1.1

--- viceversa/views.py
+++ viceversa/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 
 
-def home(requests):
-    return render(requests, 'home.html')
+def home(request):
+    return render(request, 'home.html')
 
 
-def reverse(requests):
-    return render(requests, 'reverse.html')
+def reverse(request):
+    user_text = request.GET['usertext']
+    reversed_text = user_text[::-1]
+    return render(request, 'reverse.html', {'usertext':user_text, 'reversedtext':reversed_text})
